03b37d508977e04e941ba586af5fa6ce66af9bbd
Elijah-King <edking@email.neit.edu>
2023-10-15 21:32:03 -0700

    updated project

--- canvas.js
+++ canvas.js
@@ -2,6 +2,25 @@ var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var c = canvas.getContext('2d')
-c. fillRect(100, 100, 100, 100);
-c. fillRect(400, 100, 100, 100);
-c. fillRect(300, 300, 100, 100);
+
+c.beginPath()
+c.moveTo(300,200);
+c.lineTo(400,100);
+c.lineTo(500,200);
+c.lineTo(300,200);
+c.moveTo(350,200);
+c.lineTo(250,300);
+c.lineTo(500,300);
+
+
+
+
+
+
+
+
+
+
+
+c. stroke();
+c.closePath();
