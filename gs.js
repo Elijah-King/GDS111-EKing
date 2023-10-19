98c6584edc0a9a39403720e53d6184315aa82e8b
King <edking@email.neit.edu>
2023-10-19 12:23:39 -0400

    gme statistics project

---
+++ gs.js
@@ -0,0 +1,7 @@
+console.log(prompt("Input team")); 
+console.log (    "    "         );
+
+console.log (prompt("Weapons Control"));
+console.log (prompt("Captures"));
+console.log(prompt("Ownages"));
+console.log(prompt("Efficiency"));
