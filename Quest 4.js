b0d2ddfb44391d2b4223fe569683a85d5714185f
King <edking@email.neit.edu>
2023-10-26 15:42:14 -0400

    Quest 4

---
+++ Quest 4.js
@@ -0,0 +1,34 @@
+console.log(prompt("Please input age"));
+var one = (1-17);
+
+console.log("Minor");
+console.log(prompt("Minor Please enter age again"));
+Return=console.log(prompt("Minor please enter age again"));
+Return=console.log("Minor");
+
+  
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
