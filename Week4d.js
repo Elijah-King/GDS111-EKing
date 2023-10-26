51e7a73965ebcfbbd70b6dbd79eb1f228ca23b33
King <edking@email.neit.edu>
2023-10-26 12:04:10 -0400

    more

---
+++ Week4d.js
@@ -0,0 +1,25 @@
+let fName = "Larry"; // variables are case-sensitive
+let age= 22;
+let salary=40000.00;
+let employed = true;
+
+console.log(fName + " is " + age + " years old. ");
+console.log(" He is making " + salary + " annually. ");
+console.log(typeof fName);
+console.log(typeof age);
+console.log(typeof salary);
+console.log(typeof employed);
+
+fName = 77;
+console.log("fName = " + fName);
+console.log(typeof fName);
+
+const player = ["Mario", "Luigi", "Pikachu", "Peach"]; 
+
+for(let a of player ) {
+    console.log(a)
+}
+
+
+
+
