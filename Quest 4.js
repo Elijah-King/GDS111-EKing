c2952dcc865874b8f41ea3e2c503ad9206c70eb8
Elijah-King <edking@email.neit.edu>
2023-10-30 22:31:40 -0700

    Age authenticator

--- Quest 4.js
+++ Quest 4.js
@@ -1,10 +1,40 @@
-console.log(prompt("Please input age"));
-var one = (1-17);
 
-console.log("Minor");
-console.log(prompt("Minor Please enter age again"));
-Return=console.log(prompt("Minor please enter age again"));
-Return=console.log("Minor");
+let age = 50;
+
+if  (age >=1 && age <=17) 
+    console.log ('minor');
+
+ else if (age >=18 && age <65)
+ console.log ('Neither seinor nor minor');
+
+ else if (age >=65 && age <=90)
+ console.log ('senior');
+
+  else if ( age >=100 && age <=1000)
+ console.log ('you are definitely lying!');
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   
 
