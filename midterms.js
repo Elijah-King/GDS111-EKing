bf3f6b682b83cca2b8a43bc379239ac7ffeead88
King <edking@email.neit.edu>
2023-10-31 11:05:29 -0400

    midterms

---
+++ midterms.js
@@ -0,0 +1,14 @@
+console.log (prompt(' Your options are 1-Load 2-Save 3-Play 4-Help 5-Quit or Make Selection'));
+let options= 0;
+
+if (options >=0 && options <=1 )
+console.log ('Loading Game.....');
+
+else if (options >=2 && options <3)
+console.log('Saving Game...');
+
+
+
+
+ 
+
