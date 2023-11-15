adf48e0141eaa1651319be89e8a859c0f6298415
Elijah-King <edking@email.neit.edu>
2023-11-14 20:43:03 -0800

    Quest 5

---
+++ Quest5.js
@@ -0,0 +1,59 @@
+const Arr= [" Main Menu Bar"," Change Actions","  Button Layout", " Keybindings", "  OnEvent Scripts", "  Main Menu Bar", "  Misc Options", "  Unlock Dragging", "  Show Button IDS", "  Show Action IDS", "  COPY", "  PASTE", "Select a number:_"];
+console.log(Arr);
+
+console.log(prompt("Please input desired number."));
+
+ changeActions(); {
+
+}
+
+buttonLayout(); {
+
+}
+
+keyBindings(); {
+
+}
+
+onEventScripts(); {
+
+}
+
+mainMenuBar(); {
+
+}
+
+miscOptions(); {
+
+}
+
+unlockDragging(); {
+}
+
+showButtonsIDS(); {
+
+}
+
+showActionIDS(); {
+
+}
+
+copy(); {
+
+}
+
+paste(); {
+
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
