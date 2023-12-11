61c68e3ef751a1c0132c832d47241b80cfaf7dda
King <edking@email.neit.edu>
2023-12-11 11:06:07 -0500

    TTT

--- Week9c.js
+++ Week9c.js
@@ -74,10 +74,81 @@ if (choice == 9) {
     }
 }
 return succeed;
-}
-}
-    
 
-    
+}
+
+function someonewin(symbol) {
+let win = false;
+// Horizontal Checks
+if(ttt[0][0]==symbol && ttt [0][1]==symbol && ttt[0][2]==symbol)
+win = true;
+if(ttt[1][0]==symbol && ttt [1][1]==symbol && ttt[1][2]==symbol)
+win = true;
+if(ttt[2][0]==symbol && ttt [2][1]==symbol && ttt[2][2]==symbol)
+win = true;
+//  Vertical Checks
+if(ttt[0][0]==symbol && ttt [1][0]==symbol && ttt[2][0]==symbol)
+win = true;
+if(ttt[0][1]==symbol && ttt [1][1]==symbol && ttt[2][1]==symbol)
+win = true;
+if(ttt[0][2]==symbol && ttt [1][2]==symbol && ttt[2][2]==symbol)
+win = true;
+// Diagonal Checks
+if(ttt[0][0]==symbol && ttt [1][1]==symbol && ttt[2][2]==symbol)
+win = true;
+if(ttt[0][2]==symbol && ttt [0][1]==symbol && ttt[2][0]==symbol)
+win = true;
+
+if (win) {
+    return symbol;
+}else{
+    return "?"; //no winner
+}
+}
+function getRandomInt (max) {
+    return Math.floor(Math.random() * max) + 1;
+}
+
+//main()
+let choice = 0;
+let winnerSymbol = "";
+let hasWon= false;
+do {
+    instructions();
+    // Player Moves
+    choice= Number(prompt("Enter a number of your choice:"));
+    placeChoice(choice, "X");
+    console.log("Human's Move");
+    displayBoard();
+    winnerSymbol=someoneWon("X");
+    if (winnerSymbol == "X"); {
+        console.log("Congradulations, you win!");
+        break;
+
+    } else if (winnerSymbol == "0") {
+        console.log("Sorry, you lost.");
+        break;
+    }
+    // PC Moves
+
+    let succeed = false;
+    do {
+        choice = getRandomInt(9);
+        succeed = placeChoice(choice,"0");
+    } while (!succeed);
+    console.log("PC's Move:");
+    displayBoard();
+    winnerSymbol = someoneWon("0");
+    if (winnerSymbol== "0") {
+        console.log("Sorry, you lost.");
+        break;
+    } else if (winnerSymbol =="X") {
+        console.log("Congradtulations, you win!");
+        break;
+    }
+
+} while (hasWon == false);
+
+}
 
 
