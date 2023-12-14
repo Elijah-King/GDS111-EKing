58a8c5faff673e3122c2fcf4636f6eda2d85b0e3
King <edking@email.neit.edu>
2023-12-14 09:27:50 -0500

    More

---
+++ final.js
@@ -0,0 +1,68 @@
+let player = Number
+
+console.log(prompt("Guess the number. Please input your number."));
+
+function guess(LOW) {
+console.log(prompt("You are too low guess again."));
+}
+
+function guesstwo (HIGH) {
+console.log(prompt("You are too high guess again."));
+}
+
+function guessthree(WIN) {
+console.log(prompt("You are correct you win!"));
+}
+
+
+
+ const secret = Math.floor(Math.random()*100) +1;
+console.log(secret);
+
+if (player<secret) {
+getguess()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
