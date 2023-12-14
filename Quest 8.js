16c3593c240aa5b9d3c8b55f2931a52ddf06e788
King <edking@email.neit.edu>
2023-12-14 10:37:19 -0500

    Quest 8

---
+++ Quest 8.js
@@ -0,0 +1,32 @@
+var play= 1;
+var Quit=2;
+
+var credits = 1000;
+var bets = 150;
+var winnerpaid = 0;
+var playerChoice = 0;
+
+var slots = [ [0],[0],[0],[0] 
+]
+
+
+console.log("<<<Menu>>>");
+
+console.log("<1> Spin");
+
+console.log("<2> Quit");
+
+console.log("Select One Option ");
+console.log("Credits"  + credits);
+console.log("Bets"  + bets);
+console.log("Winnerpaid" + winnerpaid);
+console.log( slots);
+
+
+
+
+
+
+
+
+
